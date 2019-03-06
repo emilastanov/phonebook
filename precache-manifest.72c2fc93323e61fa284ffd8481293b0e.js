ccf7c5d0fb03ec91ed58c43f64a7d4402a2fc343
emilastanov <emila1998@me.com>
2019-03-06 11:38:21 +0300

    1

---
+++ precache-manifest.72c2fc93323e61fa284ffd8481293b0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3a322f4947ac46fc1d3c",
+    "url": "/static/js/main.3a322f49.chunk.js"
+  },
+  {
+    "revision": "1372b4ecd3f079c272c7",
+    "url": "/static/js/2.1372b4ec.chunk.js"
+  },
+  {
+    "revision": "1372b4ecd3f079c272c7",
+    "url": "/static/css/2.26580001.chunk.css"
+  },
+  {
+    "revision": "632ff66bd137ec932947f8913025c719",
+    "url": "/index.html"
+  }
+];
