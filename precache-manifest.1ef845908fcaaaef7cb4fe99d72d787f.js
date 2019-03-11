de34bd61eb659b5ede91d67938d42d6c0a58c7a8
emilastanov <emila1998@me.com>
2019-03-11 22:29:38 +0300

    3

---
+++ precache-manifest.1ef845908fcaaaef7cb4fe99d72d787f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "99bf9948299352203958",
+    "url": "/static/js/main.e4e764c7.chunk.js"
+  },
+  {
+    "revision": "7a53393b5e6055bcca94",
+    "url": "/static/js/2.cfa91b83.chunk.js"
+  },
+  {
+    "revision": "7a53393b5e6055bcca94",
+    "url": "/static/css/2.26580001.chunk.css"
+  },
+  {
+    "revision": "c86edd567520d6d7c5f96dea941d2633",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46ed619eeaeb80260bd81104d4be0d91.js"
+  "/precache-manifest.1ef845908fcaaaef7cb4fe99d72d787f.js"
 );
 
 workbox.clientsClaim();
